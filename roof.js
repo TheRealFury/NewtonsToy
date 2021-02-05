5b524d4cc882d35dda112467ae76941cc9dca5a0
TheRealFury <76801758+TheRealFury@users.noreply.github.com>
2021-02-05 16:09:11 +0530

    Add files via upload

---
+++ roof.js
@@ -0,0 +1,24 @@
+class Roof {
+    constructor(x,y,width,height) {
+      var options = {
+          isStatic: true
+      }
+      this.width = width;
+      this.height = height;
+      this.x=x;
+      this.y=y;
+      this.body = Bodies.rectangle(x,y,width,height,options);
+      World.add(world, this.body);
+    }
+    display(){
+      var pos =this.body.position;
+      push();
+      translate(pos.x, pos.y);
+      rectMode(CENTER);
+      strokeWeight(4);
+      stroke("yellow");
+      fill(255,15,77);
+      rect(0,0, this.width, this.height);
+      pop();
+    }
+  };
